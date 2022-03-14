5f84827e763961794eed94f39a66baa1ca1d31f9
guanglun <272073020@qq.com>
2022-03-15 02:31:39 +0800

    up

--- platforms/nuttx/cmake/Platform/Generic-xtensa-esp32-elf-gcc-esp32.cmake
+++ platforms/nuttx/cmake/Platform/Generic-xtensa-esp32-elf-gcc-esp32.cmake
@@ -1,6 +1,6 @@
 
-set(cpu_flags "-mlongcalls")
+set(cpu_flags "-fno-builtin -ffunction-sections -fdata-sections -fstrict-volatile-bitfields -mlongcalls")
 
 set(CMAKE_C_FLAGS "${cpu_flags}" CACHE STRING "" FORCE)
-set(CMAKE_CXX_FLAGS "${cpu_flags}" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "${cpu_flags} -fno-exceptions -fcheck-new -fno-rtti" CACHE STRING "" FORCE)
 set(CMAKE_ASM_FLAGS "${cpu_flags} -D__ASSEMBLY__" CACHE STRING "" FORCE)
